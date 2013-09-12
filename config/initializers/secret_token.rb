cd35a9a671b3cecd9599607c5e0ccb2e34e71606
Dan Tao <daniel.tao@gmail.com>
2013-09-11 20:21:39 -0700

    bringin' the Rails back

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Budgeteer::Application.config.secret_key_base = 'f63b69bc0d03e3219f9a3611ec928a57f1eb83114f7781c96039e0d84b38873894a8db1ec476a59d2abde04a87e2703539ae437f855ef39123d041170b5becf3'
